f50dd79beed698d51908c5ee389aefff9ec1c1d3
Elgin-Skye McLaren <elginskye@Dot-Matrix.local>
2018-10-21 14:39:38 -0400

    working on adding in animation

--- dist/js/projects-script.js
+++ dist/js/projects-script.js
@@ -1,12 +1,11 @@
 function toggle_visibility(id) {
   var e = document.getElementById(id);
-  if(e.style.display == 'block')
-  e.style.display = 'none';
+  if(e.style.display == 'visible')
+  e.style.visibility = 'hidden';
   else
-  e.style.display = 'block';
+  e.style.visibility = 'visible';
 }
 function rotate_icon(id) {
   var e = document.getElementById(id);
   e.classList.toggle("fa-rotate-45");
 }
-console.log(toggle_visibility)
--- src/js/projects-script.js
+++ src/js/projects-script.js
@@ -1,9 +1,9 @@
 function toggle_visibility(id) {
   var e = document.getElementById(id);
-  if(e.style.display == 'block')
-  e.style.display = 'none';
+  if(e.style.display == 'visible')
+  e.style.visibility = 'hidden';
   else
-  e.style.display = 'block';
+  e.style.visibility = 'visible';
 }
 function rotate_icon(id) {
   var e = document.getElementById(id);
