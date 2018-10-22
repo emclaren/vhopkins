82b3df270c422666774285ad9a0807db7bc4c71c
Elgin-Skye McLaren <elginskye@Dot-Matrix.attlocal.net>
2018-10-21 22:19:22 -0400

    add transition on about page

---
+++ src/js/about-script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  let cvLink = document.getElementById('cv-link');
+  let emailLink = document.getElementById('email-link');
+
+  cvLink.style.opacity= "1";
+  emailLink.style.opacity= "1";
+
+};
+
