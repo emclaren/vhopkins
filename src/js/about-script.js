d95100690c166d0bd384f9ff2459be6cb866d0cc
Elgin-Skye McLaren <elginskye@Dot-Matrix.attlocal.net>
2018-10-22 09:09:29 -0400

    test

--- src/js/about-script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  let cvLink = document.getElementById('cv-link');
-  let emailLink = document.getElementById('email-link');
-  cvLink.style.opacity= "1";
-  emailLink.style.opacity= "1";
-
-};
-
