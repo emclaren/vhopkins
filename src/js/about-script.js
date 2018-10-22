712cf91da4d801bb633f9c3cf742517d43664fbe
Elgin-Skye McLaren <elginskye@Dot-Matrix.attlocal.net>
2018-10-21 22:25:17 -0400

    remove fade in

--- src/js/about-script.js
+++ src/js/about-script.js
@@ -1,7 +1,6 @@
 window.onload = function() {
   let cvLink = document.getElementById('cv-link');
   let emailLink = document.getElementById('email-link');
-
   cvLink.style.opacity= "1";
   emailLink.style.opacity= "1";
 
