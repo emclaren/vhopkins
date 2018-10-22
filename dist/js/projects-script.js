dca7a7752f75739e60609c75b5f96b6ad622a5e1
Elgin-Skye McLaren <elginskye@Dot-Matrix.attlocal.net>
2018-10-21 21:54:14 -0400

    add transition animations

--- dist/js/projects-script.js
+++ dist/js/projects-script.js
@@ -1,11 +1,28 @@
 function toggle_visibility(id) {
   var e = document.getElementById(id);
-  if(e.style.display == 'visible')
-  e.style.visibility = 'hidden';
-  else
-  e.style.visibility = 'visible';
+  if(e.style.visibility == 'visible'){
+    e.style.visibility = 'hidden';  
+    e.style.opacity= "0";
+    e.style.transitionDelay =".0s";
+  }else{
+    e.style.visibility = 'visible';
+    e.style.opacity= "1";
+    e.style.transitionDelay =".15s";
+  }
 }
+
 function rotate_icon(id) {
   var e = document.getElementById(id);
   e.classList.toggle("fa-rotate-45");
+}
+
+function grow(id){
+  var e = document.getElementById(id);
+  if(e.style.height == '8rem'){
+  e.style.height = '2rem';  
+  e.style.transitionDelay =".15s";
+  }else{
+  e.style.height = '8rem';
+  e.style.transitionDelay =".0s";
+  }
 }
