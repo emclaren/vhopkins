6be91b363548cd36fdd8ef643f89cb95cde4dc26
Elgin-Skye McLaren <elginskye@Dot-Matrix.attlocal.net>
2018-11-03 22:23:49 -0400

    update big pages

---
+++ dist/js/big-projects-script.js
@@ -0,0 +1,38 @@
+console.log("hi")
+window.onload = function() {
+  var e = document.getElementById('nav-projects');
+  e.classList.add("selected-nav");
+};
+
+
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'block'){
+    e.style.display= 'none';
+    e.style.opacity= "0";
+    e.style.transitionDelay =".0s";
+  }else{
+    e.style.display = 'block';
+    e.style.opacity= "1";
+    e.style.transitionDelay =".15s";
+  }
+}
+
+function rotate_icon(id) {
+  var e = document.getElementById(id);
+  e.classList.toggle("fa-rotate-45");
+}
+
+function grow(id){
+  var e = document.getElementById(id);
+  if(e.style.transitionDelay == '0s'){
+
+  e.classList.remove("expanded");
+  e.style.transitionDelay =".15s";
+  }else{
+
+  e.classList.add("expanded");
+  e.style.transitionDelay =".0s";
+  }
+}
+
